af2e094df2ec1aa2c0f3e4734d4db7e1e6358c99
rajahwu <vmradford@gmail.com>
2023-10-15 19:16:44 -0400

    style tasklist items

--- client/frontend/src/components/TodoList/index.tsx
+++ client/frontend/src/components/TodoList/index.tsx
@@ -1,10 +1,24 @@
-import { type Todo } from "@/todo.types"
-import { TodoListItem } from ".."
+import { type Todo } from "@/todo.types";
+import { TodoListItem } from "..";
 
 export default function TodoList({ todos }: { todos: Todo[] }) {
-    return (
-        <div>
-            {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
-        </div>
-    )
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>description</th>
+            <th>is complete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <TodoListItem key={todo.id} todo={todo} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- client/frontend/src/components/TodoListItem/index.tsx
+++ client/frontend/src/components/TodoListItem/index.tsx
@@ -1,13 +1,22 @@
 import { type Todo } from "@/todo.types"
 
+export function TodoListItemButtons() {
+    return (
+        <div>
+            <button className="btn btn-info">Edit</button>
+            <button className="btn btn-error">Delete</button>
+        </div>
+    )
+}
+
 export default function TodoListItem({ todo }: { todo: Todo }) {
     const { id, title, description, completed } = todo
     return (
-        <div>
-            <p>{id}</p>
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{completed}</p>
-        </div>
+            <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td>{completed ? "checked" : "not checked"}</td>
+            </tr>
     )
 }
